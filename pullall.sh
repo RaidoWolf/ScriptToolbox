d7163c34be23aa34dcef1828fe1043597a4f0635
Alexander Barber <dangerbarber@gmail.com>
2014-12-11 20:26:36 -0700

    pullall.sh - mercurial support, improved verbosity

--- pullall.sh
+++ pullall.sh
@@ -2,26 +2,56 @@
 failtoenter="0"
 for dir in `ls "$PWD"`; do
 	if [ -d "$dir" ]; then
-		cd $dir
-		if [ "$?" != "0" ]; then
-			echo "Could not enter $dir repository from $PWD. Check above for errors."
-			failtoenter="1"
-		else
-			failtoenter="0"
+
+		# GIT REPOSITORIES
+		if [ -d "$dir/.git" ]; then
+			echo "$dir is git repository. pulling..."
+			cd $dir
+			if [ "$?" != "0" ]; then
+				echo "Could not enter $dir repository from $PWD. Check above for errors."
+				failtoenter="1"
+			else
+				failtoenter="0"
+			fi
+			git pull
+			if [ "$?" != "0" ]; then
+				echo "Pull of $dir failed. Check above for errors."
+			else
+				echo "$dir updated."
+			fi
+			if [ "$failtoenter" != "1" ]; then
+				cd ..
+			fi
+			if [ "$?" != "0" ]; then
+				echo "Could not exit $dir repository. Check above for errors."
+				exit
+			fi
+
+		# MERCURIAL REPOSITORIES
+		elif [ -d "$dir/.hg" ]; then
+			echo "$dir is mercurial repository. pulling..."
+			cd $dir
+			if [ "$?" != "0" ]; then
+				echo "Could not enter $dir repository from $PWD. Check above for errors."
+				failtoenter="1"
+			else
+				failtoenter="0"
+			fi
+			hg pull
+			if [ "$?" != "0" ]; then
+				echo "Pull of $dir failed. Check above for errors."
+			else
+				echo "$dir updated."
+			fi
+			if [ "$failtoenter" != "1" ]; then
+				cd ..
+			fi
+			if [ "$?" != "0" ]; then
+				echo "Could not exit $dir repository. Check above for errors."
+				exit
+			fi
 		fi
-		git pull
-		if [ "$?" != "0" ]; then
-			echo "Pull of $dir failed. Check above for errors."
-		else
-			echo "$dir updated."
-		fi
-		if [ "$failtoenter" != "1" ]; then
-			cd ..
-		fi
-		if [ "$?" != "0" ]; then
-			echo "Could not exit $dir repository. Check above for errors."
-			exit
-		fi
+
 	fi
 done
 exit
