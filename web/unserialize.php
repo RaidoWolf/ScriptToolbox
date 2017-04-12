f931af47105ff5fc596701b32b91977a90944401
Alexander Barber <dangerbarber@gmail.com>
2017-04-12 15:22:22 -0600

    Added a PHP script for unserializing and viewing serialized data in a more human-friendly way.

---
+++ web/unserialize.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Base64 Tool</title>
+	<meta charset="utf-8" />
+</head>
+<body style="font-family:monospace;">
+<form method="post">
+	PHP Serialized Data:<textarea name="string"></textarea>
+	<input type="submit" value="submit" />
+</form>
+<br />
+<pre>
+<?php
+    if (isset($_POST['string'])) {
+        print_r(unserialize($_POST['string']));
+    }
+?>
+</pre>
+</body>
+</html>
