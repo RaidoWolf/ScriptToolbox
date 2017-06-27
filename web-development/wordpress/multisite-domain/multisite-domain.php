2055aeebe516976616aef04410d45c7cd095d1f3
Alexander Barber <dangerbarber@gmail.com>
2017-06-27 11:27:02 -0600

    Added a wordpress plugin for replacing site titles with URLs on a multi-site install.

---
+++ web-development/wordpress/multisite-domain/multisite-domain.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Plugin Name: Multisite Domain Displayer
+Description: Display the domain name of each site instead of the title.
+Version 1.0.0
+Author: Alexander Barber
+License: MIT
+ */
+
+function multisite_domain_switch () {
+
+    global $wp_admin_bar;
+
+    $blavatar = '<div class="blavatar"></div>';
+
+    foreach ((array) $wp_admin_bar->user->blogs as $blog) {
+
+        $menu_id  = 'blog-' . $blog->userblog_id;
+        $blogname = $blog->domain . $blog->path;
+        $wp_admin_bar->add_menu([
+            'parent'    => 'my-sites-list',
+            'id'        => $menu_id,
+            'title'     => $blavatar . $blogname,
+            'href'      => get_admin_url($blog->userblog_id)
+        ]);
+
+    }
+}
+
+add_action('wp_before_admin_bar_render', 'multisite_domain_switch');
